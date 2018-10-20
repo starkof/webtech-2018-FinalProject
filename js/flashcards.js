cb007ad3265335918f7cff0b8bcdd581400bdffa
starkof <saystephan@gmail.com>
2018-10-20 15:57:31 +0000

    Performing tests to implement flashcards game

---
+++ js/flashcards.js
@@ -0,0 +1,8 @@
+// Implementation of a flash cards game for learning sign language
+// by: Stephan N. Ofosuhene
+
+function wait(millis){
+    for (let i = 0; i < 10; i++) {
+        document.getElementById()
+    }
+}
